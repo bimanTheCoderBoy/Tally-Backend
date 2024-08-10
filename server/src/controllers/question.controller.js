7916beef6dd5a3359e3639bd3ca46f1403b1b99c
Biman Kumar Das <daskumarbiman2020@gmail.com>
2024-08-10 12:19:20 +0530

    test case api

--- server/src/controllers/question.controller.js
+++ server/src/controllers/question.controller.js
@@ -2,13 +2,15 @@
 import Question from '../models/Questions.model.js';
 import AsyncHandler from '../utils/AsyncHandler.js';
 import mongoose from 'mongoose';
+import ApiError from '../utils/ApiError.js';
+import {executeCoder} from '../utils/executeCoder.js';
 
 // Get all questions
 export const getAllQuestions = AsyncHandler(async (req, res) => {
   // get question tittle and dificulty level only as a form of an array of objects
 
   
-  const questions = await Question.find().select('title difficulty -_id');
+  const questions = await Question.find().select('title difficulty');
 
   // If no questions are found, return an empty array, at first when no question is created
   if (!questions.length) {
@@ -76,3 +78,57 @@ export const addQuestion = AsyncHandler(async (req, res) => {
   res.status(201).json(newQuestion);
 });
 
+
+//test code section
+export const runTestCase=AsyncHandler(async (req,res)=>{
+  const {id}=req.params;
+  const { language, code, input } = req.body;
+// console.log(language, code, input);
+
+  if(!language||!code||!input){
+    throw new ApiError('Missing required fields',400);
+  }
+  if(!id){
+      throw new ApiError('Missing ID',400);
+  }
+
+  const question=await Question.findById(id);
+
+  if(!question){
+      throw new ApiError('Question not found',404);
+  }
+  let result=[];
+  const testCases=question.testCases;
+  for(let i=0; i<testCases.length; i++){
+    const tcinput=testCases[i].input;
+    const tcoutput=testCases[i].output;
+    const actualOutput=executeCoder(language,code,tcinput);
+    if(actualOutput!=tcoutput){
+      throw new ApiError('Test case failed',400);
+    }
+    if(actualOutput==tcoutput){
+      result.push({
+        input:tcinput,
+        actualOutput:actualOutput,
+        axpectedOutput:tcoutput,
+        status:'passed'
+      });
+    }else{
+      result.push({
+        input:tcinput,
+        actualOutput:actualOutput,
+        axpectedOutput:tcoutput,
+        status:'failed'
+      });
+    }
+
+    // console.log(input, expectedOutput);
+    // let result=await runCode(language, code, input);
+    // if(result!=expectedOutput){
+    //   throw new ApiError('Test case failed',400);
+    // }
+    // console.log('Test case passed');
+  }
+res.status(200).json(result);
+});
+ 
